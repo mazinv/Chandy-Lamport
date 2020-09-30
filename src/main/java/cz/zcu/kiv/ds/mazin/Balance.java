d13bb68a907c3a44f7eb22c62d14bee3ed1f6c3d
Vit Mazin <n5ver@seznam.cz>
2020-09-30 02:25:37 +0200

    Balance lock

--- src/main/java/cz/zcu/kiv/ds/mazin/Balance.java
+++ src/main/java/cz/zcu/kiv/ds/mazin/Balance.java
@@ -3,22 +3,29 @@ package cz.zcu.kiv.ds.mazin;
 public class Balance {
 
     private long balance;
+    private final Object lock = new Object();
 
     public Balance() {
         this.balance = 100L;
     }
 
-    public synchronized long add(long amount) {
-        this.balance += amount;
-        return this.balance;
+    public long add(long amount) {
+        synchronized (lock) {
+            this.balance += amount;
+            return this.balance;
+        }
     }
 
-    public synchronized long sub(long amount) {
-        this.balance -= amount;
-        return this.balance;
+    public long sub(long amount) {
+        synchronized (lock) {
+            this.balance -= amount;
+            return this.balance;
+        }
     }
 
-    public synchronized long getBalance() {
-        return this.balance;
+    public long getBalance() {
+        synchronized (lock) {
+            return this.balance;
+        }
     }
 }
